bd9a219f50691022f949a9bb91275a79eccf6c27
KelwinHenrique <kelwin-hike@hotmail.com>
2021-01-17 11:03:37 -0300

    Added errors response

--- src/api/videos/use-cases/get-videos-by-search/get-videos-by-search.js
+++ src/api/videos/use-cases/get-videos-by-search/get-videos-by-search.js
@@ -1,5 +1,11 @@
+import { serializeError } from '../../../../core/services/serializers'
+
 const getVideosBySearch = async (cursor, query) => {
-  return { success: 'ok', cursor, query }
+  try {
+    return { success: 'ok', cursor, query }
+  } catch (error) {
+    return Promise.reject(serializeError(error, 'Error to find videos.' ))
+  }
 }
 
 export { getVideosBySearch }
